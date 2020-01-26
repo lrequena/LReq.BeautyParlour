a941f1dc48eaf310b33e13600993748c349a6d4d
Leandro Requena <leandro.requena@outlook.com>
2020-01-26 12:51:38 -0300

    Add project files.

---
+++ LReq.BeautyParlour/LReq.BeautyParlour/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using LReq.BeautyParlour.Models;
+
+namespace LReq.BeautyParlour.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
